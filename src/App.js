b6484ff91b84ac8507728401ac1725f4f44dc977
enes yilmaz <62097156+enes-yilmaz02@users.noreply.github.com>
2021-11-08 14:04:44 +0300

    LoginPage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,58 @@
 import "./styles.css";
-import Car from "./car.js"
+import React ,{useState} from 'react';
+import LoginForm from './Compenents/LoginForm'
+ function App() {
+     const adminUser={
+        email:"admin@admin.com",
+        password:"admin123"
+     }
+
+     const [user , setUser]=useState ({name: "", email:""});
+     const[error,setError]=useState("");
+     const Login=details
+         =>{
+             console.Log(details);
+
+             if(details.email== adminUser.email && details.password==adminUser.password)
+             {
+                 console.log("Logged in");
+                 setUser(
+                     {
+                         name:details.name,
+                         email:details.email
+                     }
+                 );
+             }
+             else
+             {
+               
+                 setError("details do not match! ")
+             }
+         }
+         const Logout=()=>{
+             
+             setUser ({name: "", email:""});
+         }
+     
+  return (
+    <div className="App">
+      
+     {(user.email != "")}?(
+         <div className="welcome">
+          
+          <h2>Welcome , <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+
+
+         </div>
+     ) :(
+         <LoginForm Login={Login} error={error}/>
+     )
 
 
 
+    </div>
 
- const mycar = new Model("Ford", "Mustang");
- 
+  );
+}
+export default App;
